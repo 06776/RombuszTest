daae2a1d6a11a2f985068bbc5fe95cbb8c19704a
06776 <hajdarapatrik11@outlook.com>
2024-01-10 10:37:56 +0100

    Test_5 - Create testCalculateArea

--- src/test/java/RombuszTest.java
+++ src/test/java/RombuszTest.java
@@ -1,12 +1,18 @@
 import lan.zold.Rombusz;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class RombuszTest {
-    
-    Rombusz rombusz;
 
-    @BeforeTest
-    public void setUp() {
-        this.rombusz = new Rombusz();
-    }
+  Rombusz rombusz;
+
+  @BeforeTest
+  public void setUp() {
+    this.rombusz = new Rombusz();
+  }
+
+  @Test
+  public void testCalculateArea() {
+    
+  }
 }
