bb0d90aa1da6b5fd10153d7d5de7d07b0b6e1124
06776 <hajdarapatrik11@outlook.com>
2024-01-10 10:43:31 +0100

    Test_7 - Create calculateArea in Rombusz.java

--- src/main/java/lan/zold/Rombusz.java
+++ src/main/java/lan/zold/Rombusz.java
@@ -1,5 +1,11 @@
 package lan.zold;
 
 public class Rombusz {
-    
+
+  public double calculateArea(double sideLength, double alpha) {
+    double beta = 180 - alpha;
+    double radAlpha = Math.toRadians(alpha);
+    double radBeta = Math.toRadians(beta);
+    return Math.pow(sideLength, 2) * Math.sin(radAlpha);
+  }
 }
