143597c51e99763798846b61822cbf6b1cdd736a
06776 <hajdarapatrik11@outlook.com>
2024-01-10 10:41:54 +0100

    Test_6 - Create test method in calculateArea

--- src/test/java/RombuszTest.java
+++ src/test/java/RombuszTest.java
@@ -1,4 +1,6 @@
 import lan.zold.Rombusz;
+
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -12,7 +14,11 @@ public class RombuszTest {
   }
 
   @Test
-  public void testCalculateArea() {
-    
+  public void testCalculateAreaSuccess() {
+    double sideLength = 5.0;
+    double alpha = 60.0;
+    double expectedArea = Math.pow(sideLength, 2) * Math.sin(Math.toRadians(alpha));
+    double actualArea = rombusz.calculateArea(sideLength, alpha);
+    Assert.assertEquals(actualArea, expectedArea, 0.1, "Hibas terulet szamitas");
   }
 }
